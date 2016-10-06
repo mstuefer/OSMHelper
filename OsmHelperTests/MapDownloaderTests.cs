c464734e8a1d341dc6595aa2348e23fc5e191c55
mstuefer <mstuefer@gmail.com>
2016-10-06 11:17:29 +0200

    Add MapDownloaderTest

---
+++ OsmHelperTests/MapDownloaderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsmHelper;
+
+namespace OsmHelperTests
+{
+    [TestClass]
+    public class MapDownloaderTests
+    {
+        [TestMethod]
+        public void DownloadTest()
+        {
+            const string continent = "EuRope";
+            const string country = "AndoRRA";
+            const string filename = "EuRope-AndoRRA-latest.osm.bz2";
+
+            MapDownloader.Download(continent, country, filename);
+
+            Assert.IsTrue(File.Exists(filename), $"File {filename} was not Downloaded");
+        }
+    }
+}
