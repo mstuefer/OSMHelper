229e0eada34e271421cb4485b4f20b2cfe8fe89a
mstuefer <mstuefer@gmail.com>
2016-10-06 11:16:57 +0200

    Add MapHandler interface
      To facilitate the handling of the Maps (present on disk / to download etc) a MapHandler with the given Interface has to be implemented

---
+++ OsmHelperInterfaces/IMapHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace OsmHelperInterfaces
+{
+    public interface IMapHandler
+    {
+        void DeleteCachedMaps();
+        FileStream GetOsmMap();
+        void RefreshMap();
+    }
+}
