93f75f85df4fc3e4111f31dfbdd4bbfed2112471
mstuefer <mstuefer@gmail.com>
2016-10-06 11:21:42 +0200

    Add static DistanceCalculator
      The DistanceCaclulator so far implements the Harvesine Formula to calculate the distance between two 'near' points (rather precisley).

---
+++ OsmHelper/DistanceCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OsmHelperInterfaces;
+
+namespace OsmHelper
+{
+    /// <summary>
+    /// Calculates distance between two given INodes
+    /// </summary>
+    internal static class DistanceCalculator
+    {
+        /// <summary>
+        /// Returns distance (Harvesine) between two given INodes
+        /// </summary>
+        /// <param name="srcNode"></param>
+        /// <param name="dstNode"></param>
+        /// <returns></returns>
+        public static double GetDistance(INode srcNode, INode dstNode)
+        {
+            return Harvesine(srcNode.Latitude, srcNode.Longitude, dstNode.Latitude, dstNode.Longitude);
+        }
+
+        private static double Harvesine(double latitude1, double longitude1, double latitude2, double longitude2)
+        {
+            const int meanEarthRadius = 6371;
+            var deltaLatitude = ToRadians(latitude2 - latitude1);
+            var deltaLongitude = ToRadians(longitude2 - longitude1);
+            latitude1 = ToRadians(latitude1);
+            latitude2 = ToRadians(latitude2);
+
+            var a = Math.Sin(deltaLatitude / 2) * Math.Sin(deltaLatitude / 2) + 
+                Math.Sin(deltaLongitude / 2) * Math.Sin(deltaLongitude / 2) * 
+                Math.Cos(latitude1) * Math.Cos(latitude2);
+            return meanEarthRadius * 2 * Math.Asin(Math.Sqrt(a));
+        }
+
+        private static double ToRadians(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+    }
+}
