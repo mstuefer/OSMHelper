7e9b4b5ae79b80600be522771227d898537faf4d
mstuefer <mstuefer@gmail.com>
2016-10-06 11:09:05 +0200

    Add interface to represent the resulting graph

---
+++ OsmHelperInterfaces/IGraph.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace OsmHelperInterfaces
+{
+    public interface IGraph
+    {
+        void GetFromXmlStream(Stream osmXml);
+        IGraph AddNodeToGraph(long id, float lat, float lon);
+        IGraph AddEdge(long srcNodeId, long dstNodeId, double distance, bool oneWay);
+        long CountNodes();
+        void Get(string continent, string country, bool deleteCachedMaps);
+        long CountEdges();
+        IDictionary<long,LinkedList<INode>> OsmGraph { get; }
+    }
+}
