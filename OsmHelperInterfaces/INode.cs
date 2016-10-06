597152a147e9f61c49b7a06c957d5b3a70ee020a
mstuefer <mstuefer@gmail.com>
2016-10-06 11:14:58 +0200

    Add interface to represent Ways and Nodes
      The term Ways comes from the OsmXml, where each way consists of a lists of nd-tags, and each nd-tag has a reference to a node-id. The Node (with the given id) then holds the latitude and longitude necessary afterwards to calculate the distancel/length of a way.
      In case the node is used in the adjacency list (as a neighbor) we also need the property Distance. Furthermore it might happen that a node is actually a street crossing point, which is why we have to count (in the Counter) how often a node appears in some ways.

---
+++ OsmHelperInterfaces/INode.cs
@@ -0,0 +1,11 @@
+﻿namespace OsmHelperInterfaces
+{
+    public interface INode
+    {
+        long Id { get; set; }
+        double Distance { get; set; } // in km
+        int Counter { get; set; }
+        float Latitude { get; set; }
+        float Longitude { get; set; }
+    }
+}
---
+++ OsmHelperInterfaces/IWay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace OsmHelperInterfaces
+{
+    public interface IWay
+    {
+        long Id { get; set; }
+        List<long> NdsList { get; set; }
+        bool OneWay { get; set; }
+    }
+}
